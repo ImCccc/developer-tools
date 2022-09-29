d69361c2b47fa5675f26c6517854fc420e160140
licr1 <licr1@infore.com>
2022-09-29 16:43:13 +0800

    feat: 删除多余文件

--- test/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import App from './app';
-
-import './index.css';
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
